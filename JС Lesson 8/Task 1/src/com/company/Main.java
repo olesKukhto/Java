fb5b6061aeecc49c22c9e12df024dbc07d50475d
olesKukhto <68856835+olesKukhto@users.noreply.github.com>
2020-09-17 01:13:24 +0300

    17.09

--- JС Lesson 8/Task 1/src/com/company/Main.java
+++ JС Lesson 8/Task 1/src/com/company/Main.java
@@ -1,59 +1,98 @@
 package com.company;
 
+import java.time.Month;
+import java.util.Arrays;
 import java.util.Scanner;
+import java.util.Spliterator;
+import java.util.stream.Stream;
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(Months.values());
         Scanner scanner = new Scanner(System.in);
-        String someString = new String();
+        String someString;
         someString = scanner.nextLine();
         someString = someString.toUpperCase();
-        System.out.println(Months.APRIL);
-        System.out.println(Months.valueOf("APRL"));
+        try {
+            Months.valueOf(someString);
+        } catch (IllegalArgumentException e) {
+            System.out.println("Немає такого місяця");
+            System.exit(0);
+        }
+        System.out.println();
+        for (Months day : Months.values()) {
+            if (day == Months.valueOf(someString)) {
+                System.out.println("Є ");
+                for (Months month : Months.values()) {
+                    if (Months.valueOf(someString).getSeasons() == month.getSeasons()) {
+                        System.out.println(month.name() + " Одинакова пора року ");
+                    }
+                }
+                System.out.println();
+                for (Months month : Months.values()) {
+                    if (Months.valueOf(someString).getDay() == month.getDay()) {
+                        System.out.println(month.name() + " Одинакова кількість днів ");
+                    }
+                }
+            }
 
 
+        }
+        System.out.println();
+        for (Months months : Months.values()) {
+            if (Months.valueOf(someString).getDay() > months.getDay()) {
+                System.out.println("В місяці -> " + someString + " більше днів ніж в " + months.name());
+            }
+        }
+        System.out.println();
+        for (Months months : Months.values()) {
+            if (Months.valueOf(someString).getDay() < months.getDay()) {
+                System.out.println("В місяці -> " + someString + " менше днів ніж в " + months.name());
 
+            }
 
-//        switch (someString) {
-//            case "january":
-//
-//                break;
-//            case "february":
-//
-//                break;
-//            case "march":
-//
-//                break;
-//            case "april":
-//
-//                break;
-//            case "may":
-//
-//                break;
-//            case "june":
-//
-//                break;
-//            case "july":
-//
-//                break;
-//            case "august":
-//
-//                break;
-//            case "september":
-//                ;
-//            case "october":
-//
-//                break;
-//            case "november":
-//                ;
-//            case "december":
-//
-//                break;
-//            default:
-//
-//                break;        }
-//    }
+        }
+        System.out.println();
+        switch (Months.valueOf(someString).getSeasons()){
+            case AUTUMN:
+                System.out.println("Winter is coming");
+                break;
+            case SPRING:
+                System.out.println("Наступна пора року Літо ");
+                break;
+            case SUMMER:
+                System.out.println("Наступна пора року Осінь");
+                break;
+            case WINTER:
+                System.out.println("Наступна пора року Весна");
+        }
+        switch (Months.valueOf(someString).getSeasons()){
+            case AUTUMN:
+                System.out.println("Попередня пора року Літо");
+                break;
+            case SPRING:
+                System.out.println("Попередня пора року Зима ");
+                break;
+            case SUMMER:
+                System.out.println("Попередня пора року Весна ");
+                break;
+            case WINTER:
+                System.out.println("Попередня пора року Осінь");
+                break;
+
+    }
+        System.out.println();
+        for (Months months: Months.values()) {
+            if(months.getDay() % 2 == 1){
+                System.out.println("В місяці "+months+" не парна кількість днів");
+            }
+        }
+        System.out.println();
+        for (Months m: Months.values()) {
+            if(m.getDay()%2 == 0 ){
+                System.out.println("В місяці "+m+" парна кількість днів");
+            }
+        }
+             }
 }
-}
+
