e72a151c9d5d944c0acf7a84d8fea29361b02346
olesKukhto <68856835+olesKukhto@users.noreply.github.com>
2020-09-20 21:10:28 +0300

    20.09

--- JC Lesson 11/Task 1/src/com/company/Main.java
+++ JC Lesson 11/Task 1/src/com/company/Main.java
@@ -1,18 +1,24 @@
 package com.company;
 
-import java.util.ArrayList;
-import java.util.Random;
+import java.util.Collections;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
         ArrayList<Integer> integer = new ArrayList();
+        List<Integer> list = new ArrayList();
         Random random = new Random();
-        int rand = random.nextInt(10) + 1;
-        for (int i = 0; i < rand;i++){
-            integer.add(random.nextInt());
+        int rand = random.nextInt() + 1;
+        for (int i = 0; i < 20;i++){
+            integer.add(random.nextInt(100) + 1);
         }
         int size = integer.size();
-        
+        Collections.sort(integer);
+        Collections.reverse(integer);
+
+        for(int i = 0;i < size;i++){
+            System.out.println(integer.get(i));
+        }
     }
 }
